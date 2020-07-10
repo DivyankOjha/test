d20e846391ab969e48eeeeff0db530b67a18fd0c
Divyank Ojha <54564161+DivyankOjha@users.noreply.github.com>
2020-07-10 19:52:17 +0530

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,5 @@
 const nodemailer = require('nodemailer');
-// module.exports = class Email {
-//   constructor(user, url) {
-//     this.to = user.email;
-//     this.firstName = user.name.split(' ')[0];
-//     this.url = url;
-//     this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
-//   }
 
-//   newTransport() {
-//     if (process.env.NODE_ENV === 'production') {
-//       // Sendgrid
-//       return nodemailer.createTransport({
-//         service: 'SendGrid',
-//         auth: {
-//           user: process.env.SENDGRID_USERNAME,
-//           pass: process.env.SENDGRID_PASSWORD
-//         }
-//       });
-//     }
 const sendEmail = async (options) => {
   //1 transporter
   const transporter = nodemailer.createTransport({
