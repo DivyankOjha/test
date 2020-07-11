9275b70ef1106c892e05754c24bc6ad8b994b5e6
Divyank Ojha <54564161+DivyankOjha@users.noreply.github.com>
2020-07-11 13:03:46 +0530

    Added logout and Security
    Added logout and Security

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const helmet = require('helmet');
 const userRouter = require('./routes/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
-
 // 1) MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet());
+
+//development logging
 // if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
+//   app.use(morgan());
 // }
+
 app.use(morgan('dev'));
+
+//body parser - reading data from the body into req.body
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }, { limit: '50mb' }));
 
+//Data Sanitization against NoSql Query Injection
+app.use(mongoSanitize());
+//Data sanitization against xss
+
+//app.use(express.json({ limit: '10kb' }));
 app.use(express.json());
+
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
@@ -23,6 +38,7 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,13 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.get('/confirmation/:token', authController.activateAccount);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+// Protect all routes after this middleware
+//router.use(authController.protect);
 router.patch(
   '/updateMyPassword',
   authController.protect,
