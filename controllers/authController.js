fb2e2bce306b6193608ef232508a167a9a4e63c5
Divyank Ojha <54564161+DivyankOjha@users.noreply.github.com>
2020-07-14 13:41:28 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,12 +81,12 @@ exports.signup = catchAsync(async (req, res, next) => {
     //   '.\n',
   };
   await transporter.sendMail(mailOptions, function (err) {
-    if (err) {
-      return res.status(500).send({ msg: err.message });
-    }
-    //res
-    // .status(200)
-    //  .send('A verification email has been sent to ' + user.email + '.');
+    // if (err) {
+    //   return res.status(500).send({ msg: err.message });
+    // }
+    res
+      .status(200)
+      .send('A verification email has been sent to ' + user.email + '.');
   });
 
   // res.status(201).json({
