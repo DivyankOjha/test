e04551c6243258fcc5bfdf6c14e37bc84beae233
Divyank Ojha <54564161+DivyankOjha@users.noreply.github.com>
2020-07-15 12:49:30 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,13 +82,16 @@ exports.signup = catchAsync(async (req, res, next) => {
   };
   transporter.sendMail(mailOptions, function (err) {
     if (err) {
-      return res.status(500).send({ msg: err.message });
+      console.log('ERRor sending mail: ' + err);
+    } else {
+      console.log('Mail sent to: ' + newUser.email);
     }
-   // res
-      //.status(200)
-     // .send('A verification email has been sent to ' + newUser.email + '.');
-    // .send('A verification email has been sent to ' + user.email + '.');
   });
+  //  res
+  //    .status(200)
+  // .send('A verification email has been sent to ' + newUser.email + '.');
+  // .send('A verification email has been sent to ' + user.email + '.');
+
   createSendToken(newUser, 201, res);
 
   // res.status(201).json({
